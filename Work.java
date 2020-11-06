e07188815580d3a5576b4363355adea74356873e
KavitaMyaginamani <72960895+KavitaMyaginamani@users.noreply.github.com>
2020-11-06 18:27:12 +0530

    thirdexample commit

---
+++ Work.java
@@ -0,0 +1,17 @@
+class Worksname
+{
+public static void main(String[] work)
+{
+System.out.println("kavita");
+music();
+}
+public static void music()
+{
+System.out.println(" javaliesning a lyrics");
+drinking();
+}
+public static void drinking()
+{
+System.out.println(" drinking Tea");
+}
+}

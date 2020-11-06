c68fa9b64ff293b453a590372aa469be74a00ed9
KavitaMyaginamani <72960895+KavitaMyaginamani@users.noreply.github.com>
2020-11-06 12:19:04 +0530

    arrayString commit

---
+++ wing.java
@@ -0,0 +1,22 @@
+public class Wing 
+{
+ public static void main(String[] states)
+ {
+	 System.out.println("invoked main");
+	 System.out.println("states "+states);
+	 int sizeOfMainParam=states.length;
+	 
+	 System.out.println("sizeOfMainParam "+sizeOfMainParam);
+	 System.out.println("Element @ Index 0 "+states[0]);
+	 System.out.println("Element @ Index 1 "+states[1]);
+	 System.out.println("Element @ Index 2 "+states[2]);
+	 System.out.println("Element @ Index 3 "+states[3]);
+	 System.out.println("Element @ Index 4 "+states[4]);
+	 System.out.println("Element @ Index 5 "+states[5]);
+	 System.out.println("Element @ Index 6 "+states[6]);
+	 System.out.println("Element @ Index 7 "+states[7]);
+	 System.out.println("Element @ Index 8 "+states[8]);
+	 System.out.println("exit:: main");
+	 
+ }
+ }

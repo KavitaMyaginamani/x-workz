12930b21a630995e1d9db876ccdc1aebd7f2be67
KavitaMyaginamani <72960895+KavitaMyaginamani@users.noreply.github.com>
2020-11-20 11:43:36 +0530

    eclipse examples

---
+++ bus/BusSearch.java
@@ -0,0 +1,12 @@
+package com.xworkz.bus;
+
+public class BusSearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("invoked main");
+		Place.search("Hubli");
+System.out.println("Exit :: main");
+	}
+
+}
---
+++ hotel/SearchTester.java
@@ -0,0 +1,11 @@
+package com.xworkz.hotel;
+
+public class SearchTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Total items to search" + Food.totalItems());
+		Food.search("Idli");
+	}
+
+}

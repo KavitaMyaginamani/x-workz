a084c31dd4da75dafe0d7d52177a234775c22572
KavitaMyaginamani <72960895+KavitaMyaginamani@users.noreply.github.com>
2020-11-20 12:12:45 +0530

    Add files via upload

---
+++ phone-directory/src/com/xworkz/phonedirectory/NumberSearch.java
@@ -0,0 +1,11 @@
+package com.xworkz.phonedirectory;
+
+public class NumberSearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Total items to search" + PhoneNumber.totalItems());
+		PhoneNumber.search(9980297429l);
+	}
+
+}
